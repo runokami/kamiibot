918b36665631875484ff21b3f92f9550aa8d51ed
runokami <98051960+runokami@users.noreply.github.com>
2025-03-18 18:00:42 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const token = process.env.DISCORD_TOKEN;  // Token'ı çevresel değişken olarak al
+
+client.once('ready', () => {
+  console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(token);
